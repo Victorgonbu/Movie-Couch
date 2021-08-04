12ff334d0bce03b51eb88eb69548db0dc8bf4062
VMB <peter200970@hotmail.com>
2021-08-04 14:37:43 -0500

    Create useDidMountEffect custom hook and implemented it in SearchBox componen

--- src/components/containers/SearchBox.js
+++ src/components/containers/SearchBox.js
@@ -1,12 +1,30 @@
-import { searchBox, searchButton, searchBar } from '../../styles/Navbar.module.css';
+import { searchBox, searchButton, searchBar, activeBox } from '../../styles/Navbar.module.css';
+import { useRef } from 'react';
+import useDidMountEffect from '../../hooks/useDidMountEffect';
 
-const SearchBox = () => {
+import { connect } from 'react-redux';
+
+const SearchBox = (props) => {
+    const { searchActive } = props;
+    const searchRef = useRef(null);
+
+    useDidMountEffect(() => {
+        searchRef.current.classList.toggle(activeBox);
+    }, [searchActive])
+
     return(
-        <div className={searchBox}>
+        <div ref={searchRef} className={searchBox}>
             <input placeholder="Type movie title" className={searchBar} />
             <button type="button" className={searchButton}>Search</button>
         </div> 
     )
 };
 
-export default SearchBox;
+const mapStateToProps = (state) => {
+    return {
+        searchActive: state.filter.searchActive
+    }
+};
+
+
+export default connect(mapStateToProps)(SearchBox);
