5631d907087b054e7edfc24d881c2c8bb4c654dd
VMB <peter200970@hotmail.com>
2021-08-11 14:15:02 -0500

    Add proptype validations to Movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,6 @@
 import ReactStars from 'react-rating-stars-component';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import {
   listItem, poster, itemDetails, title, date, moreDetails, starRate, rating,
 } from '../styles/Home.module.css';
@@ -33,6 +34,14 @@ const Movie = (props) => {
       </div>
     </li>
   );
+};
+
+Movie.propTypes = {
+  value: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool,
+    PropTypes.array,
+    PropTypes.number])).isRequired,
 };
 
 export default Movie;
