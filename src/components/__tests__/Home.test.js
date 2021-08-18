28b92e8d551d7ddd927777d27d097e6d1f69519b
VMB <peter200970@hotmail.com>
2021-08-17 21:44:06 -0500

    Refactor Home test to use redux-mock-store instead of real store

--- src/components/__tests__/Home.test.js
+++ src/components/__tests__/Home.test.js
@@ -2,23 +2,26 @@ import React from 'react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import {
-  render, waitFor, screen,
+  render, waitFor, screen, reduxStore,
 } from '../utils/test-utils';
 import '@testing-library/jest-dom/extend-expect';
+import { movieURL } from '../../API';
 import Home from '../containers/Home';
 
 /* eslint-disable react/display-name */
 jest.mock('../MovieThumb', () => () => <div data-testid="movie" />);
 
+const content = [
+  {
+    id: 1,
+    title: 'Interstellar',
+    poster_path: '/posterimage.png',
+  },
+];
+
 const server = setupServer(
   rest.get('https://api.themoviedb.org/3/movie/popular', (req, res, ctx) => res(ctx.json({
-    results: [
-      {
-        id: 1,
-        title: 'Interstellar',
-        poster_path: '/posterimage.png',
-      },
-    ],
+    results: content,
   }))),
 );
 
@@ -27,30 +30,38 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe('Home', () => {
-  describe('Fetch movies from API if filter given', () => {
-    it('Display if movie poster_path field retrieved', async () => {
-      render(<Home />);
-      await waitFor(() => expect(screen.getByTestId('movie')).toBeInTheDocument());
-      expect(await screen.getByTestId('movie')).toBeInTheDocument();
+  describe('Fetch popular movies from API on first render', () => {
+    it('dispatch actions to redux store to retrieve and store data', async () => {
+      render(<Home />, {
+        filter: {
+          content: null,
+          current: 'Popular',
+          contentURL: null,
+        },
+      });
+      const actions = reduxStore.getActions();
+      await waitFor(() => expect(actions.length).toBe(2));
+      expect(actions[0]).toEqual({ type: 'SET_CONTENT_URL', payload: `${movieURL}popular?` });
+      expect(actions[1]).toEqual({ type: 'SET_CURRENT_CONTENT', payload: content });
     });
 
-    it('Does not display if movie poster_path field not retrieved', async () => {
-      server.use(
-        rest.get('https://api.themoviedb.org/3/movie/popular', (req, res, ctx) => res(ctx.json({
-          results: [
-            {
-              id: 1,
-              title: 'Interstellar',
-              poster_path: null,
-            },
-          ],
-        }))),
-      );
+    it('render retrieved content passed as props using MovieThumb component', () => {
+      render(<Home />, {
+        filter: {
+          content,
+        },
+      });
+      expect(screen.getByTestId('movie')).toBeInTheDocument();
+    });
 
-      render(<Home />);
-      await waitFor(() => expect(screen.getByText('No matches found')).toBeInTheDocument());
-      expect(await screen.queryByTestId('movie')).not.toBeInTheDocument();
-      expect(await screen.getByText('No matches found')).toBeInTheDocument();
+    it('Does not render content if no data is retrieved or valid', () => {
+      render(<Home />, {
+        filter: {
+          content: [],
+        },
+      });
+      expect(screen.queryByTestId('movie')).not.toBeInTheDocument();
+      expect(screen.getByText('No matches found')).toBeInTheDocument();
     });
   });
 });
--- src/components/utils/test-utils.js
+++ src/components/utils/test-utils.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import configureStore from '../../reducers/reduxStore';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+let reduxStore;
 
 function render(
   ui,
   {
     preloadedState,
-    store = configureStore(),
+    store = configureStore([thunk]),
     ...renderOptions
   } = {},
 ) {
+  reduxStore = store(renderOptions);
   // eslint-disable-next-line react/prop-types
   function Wrapper({ children }) {
-    return <Provider store={store}><Router>{children}</Router></Provider>;
+    return <Provider store={reduxStore}><Router>{children}</Router></Provider>;
   }
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 }
@@ -22,4 +26,4 @@ function render(
 // re-export everything
 export * from '@testing-library/react';
 // override render method
-export { render };
+export { render, reduxStore };
